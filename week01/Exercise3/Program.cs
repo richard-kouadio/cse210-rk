125426597ce2d841873b9afa0e17f09e8e9b7455
Richard-Kouadio <Richardkouamek@gmail.com>
2026-05-11 15:57:27 +0000

    Completed Exercie 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
  { 
+    Console.Write("Enter a number: ");
+    string input = Console.ReadLine();
+    int number = int.Parse(input);
+    
     for (int i = 0; i < 5; i++)
     {
         Console.WriteLine(i);
