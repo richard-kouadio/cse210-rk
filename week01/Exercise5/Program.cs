683f1f01dc36cb3763f0c7fd50811a9cf001302e
Richard-Kouadio <Richardkouamek@gmail.com>
2026-05-12 11:24:59 +0000

    Completed Exercice 4

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,29 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine ("Welcome to the program!");
+    }
+   
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name ");
+        string name = Console.ReadLine();
+        return name;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int number = PromptUserNumber();
     }
+    static int PromptUserNumber()
+    {
+    Console.Write("Please enter your favorite number: ");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+    }
+        
 }
