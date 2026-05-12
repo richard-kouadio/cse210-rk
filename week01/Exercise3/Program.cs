5b682c8a9b3d926ea4ef695c95b0c0ba3c9fac3d
Richard-Kouadio <Richardkouamek@gmail.com>
2026-05-12 17:24:44 +0000

    Completed Exercice 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,14 +3,29 @@ using System;
 class Program
 {
     static void Main(string[] args)
- { 
-    Console.Write("Enter a number: ");
-    string input = Console.ReadLine();
-    int number = int.Parse(input);
-    
-    for (int i = 0; i < 5; i++)
     {
-        Console.WriteLine(i);
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
- }
 }
