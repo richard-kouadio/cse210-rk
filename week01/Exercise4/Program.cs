6c01862f86b0699b0c974b8b00ff0efe1a3de5b0
Richard-Kouadio <Richardkouamek@gmail.com>
2026-05-12 10:16:34 +0000

    Completed Exercice 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -17,7 +17,28 @@ class Program
             {
                 number.Add(userNumber);
             }
+            
         } 
+        int sum = 0;
 
+            foreach (int num in number)
+            {
+                sum += num;
+            
+            } 
+
+            Console.WriteLine($"The sum is: {sum}");
+            float average = ((float)sum) / number.Count;
+            Console.WriteLine ($"The average is: {average}");
+            int largest = number[0];
+            foreach (int num in number)
+        {
+            if (num > largest)
+            {
+                largest = num;
+            }
+        }
+
+        Console.WriteLine ($"The largest number is: {largest}");
     }
 }
