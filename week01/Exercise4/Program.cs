e9d537747bde74e2e728ed46ba0b38542f2de9d1
Richard-Kouadio <Richardkouamek@gmail.com>
2026-05-12 08:59:38 +0000

    Completed Exercice 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,24 @@
 using System;
-
+using System.Collections.Generic;
+using System.Globalization;
+using System.Transactions;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+       List<int> number = new List<int>();
+       int userNumber = -1;
+       while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            userNumber = int.Parse(input);
+
+            if (userNumber != 0)
+            {
+                Numbers.Add(userNumber);
+            }
+        } 
+
     }
 }
